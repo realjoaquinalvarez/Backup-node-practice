fc02d1f954e7bcf2267caa3da7ef74e15bc106c3
Joaquin <clcflecha@gmail.com>
2024-06-21 15:39:50 -0400

    copia de seguridad

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,28 @@ const port = process.env.PORT || 4000;
 server.listen(port, () => {
     console.log(colors.blue.bold(`Hola mundo en consola, desde el puerto, ${port}`));
 });
+
+type Product = {
+    id: number;
+    price: number;
+    name: string;
+}
+
+let productname = 'Tablet';
+let isAuth = false;
+let price = 30;
+
+let product : Product = {
+    id: 1,
+    price: 30,
+    name: 'Tablet'
+};
+
+let product2 : Product = {
+    id: 1,
+    price: 30,
+    name: 'Tablet'
+};
+
+const numbers = [10, 20, 30];
+
