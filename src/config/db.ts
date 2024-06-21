f95dbc1b702cd573fb297adc98dd8347c5b9728c
Joaquin <clcflecha@gmail.com>
2024-06-21 10:38:22 -0400

    copia de seguridad

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
+import colors from 'colors';
 
 export const connectDB = async () => {
     try {
-        const url = 'mongodb+srv://root:PHlhy8bXJKkLyE26@cluster0.o460o.mongodb.net/linktree_node_typescript';
-        const connection = await mongoose.connect(url);
-        console.log(connection);
+        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+        const url = `${connection.host}:${connection.port}`
+        // console.log(connection);
 
-        console.log('MongoDB Conectado');
+        console.log(colors.cyan.bold('MongoDB Conectado en' + url));
     } catch (error) {
-        console.log(error);
+        console.log(colors.bgRed(error));
+        process.exit(1);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors'
 import server from './server'
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log('Hola mundo en consola, desde el puerto', port);
+    console.log(colors.blue.bold(`Hola mundo en consola, desde el puerto, ${port}`));
 });
