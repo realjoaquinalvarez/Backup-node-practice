9e9db19d3b7fc49dc5e652b1233c87969299563d
Joaquin <clcflecha@gmail.com>
2024-06-22 10:36:45 -0400

    copia de seguridad

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,13 @@
+import { Request, Response } from 'express';
 import User from "../models/User";
 
-export const createAccount = async(req, res) => {
+export const createAccount = async(req : Request, res : Response) => {
+    const { email } = req.body;
+
+    console.log( email )
+
+    return;
+    
     const user = new User(req.body);
     
     await user.save();
