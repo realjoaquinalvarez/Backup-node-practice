246c8f625c3de26825f66f602016765a62319186
Joaquin <clcflecha@gmail.com>
2024-06-24 17:52:08 -0400

    copia de seguridad

--- src/middleware/validations.ts
+++ src/middleware/validations.ts
@@ -1,13 +1,14 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { validationResult } from 'express-validator';
 
-export const handleInputErrors = ( req : Request, res : Response ) => {
+export const handleInputErrors = ( req : Request, res : Response, next: NextFunction) => {
 
+    console.log('desde validation.ts');
     // manejar errores
     let errors = validationResult(req);
     if(!errors.isEmpty()){
         res.status(400).json({error: errors.array()});
         return;
     }
-    
+    next();
 };
