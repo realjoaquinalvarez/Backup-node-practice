8d4d388c334c89ebaa0097b7ae6e1361dae7cd67
Joaquin <clcflecha@gmail.com>
2024-06-24 08:24:34 -0400

    copia de seguridad

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import slug from 'slug';
+import slugify from 'slugify';
 import User from "../models/User";
 import { hashPassword } from '../utils/auth';
 
@@ -9,13 +9,27 @@ export const createAccount = async(req: Request, res: Response) => {
 
     const userExists = await User.findOne({email});
     if(userExists){
-        const error = new Error('El usuario ya esta registrado');
+        const error = new Error('El correo ya esta en registrado');
+        res.status(409).json({error : error.message});
+        return;
+    }
+
+    const handle = slugify(req.body.handle, {
+        lower: true,    
+        trim: true,    
+        replacement: '' 
+    });
+
+    const handleExist = await User.findOne({handle});
+    if(handleExist){
+        const error = new Error('El nombre de usuario ya esta en uso');
         res.status(409).json({error : error.message});
         return;
     }
     
     const user = new User(req.body);
     user.password = await hashPassword(password);
+    user.handle = handle;
     await user.save();
     res.status(201).send('Registro creado correctamente');
 };  
